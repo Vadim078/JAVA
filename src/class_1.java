22fe3633d088716a135461173b01976bfde91d39
venil <morganf@bk.ru>
2022-12-09 15:42:06 +0500

    123

---
+++ src/class_1.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+public class class_1 {
+    public static void main(String [] args){
+    for(int i = 0;i<=10;i++){       //i++ ==i = i+1 (true)
+        System.out.println("Hello "+i);
+    }
+    }
+}
